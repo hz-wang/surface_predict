61f6634fdbebf28886b055bacfa88665e3e39f33
hz-wang <wanghaozhe1994@hotmail.com>
2018-04-11 17:22:26 +0800

    html test

--- flask_test.py
+++ flask_test.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('helloworld.html', name=name)
 
 if __name__ == '__main__':
     app.run()
